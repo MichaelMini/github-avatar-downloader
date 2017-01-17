8a1a3c8c570e33be06d4aa5e1a97fab9d241e87d
MichaelMini <mcho911@gmail.com>
2017-01-17 07:22:10 +0000

    Step8: Connect the functions completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,5 @@
 var request = require('request');
-
+var fs = require('fs');
 var GITHUB_USER = "MichaelMini";
 var GITHUB_TOKEN = "c36ec186c9a3ad5104eb2dd2c83ba812b897bec9";
 
@@ -10,15 +10,42 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var requestOptions = {
   	url: requestURL,
   	headers: {
-  		'user-agent': GITHUB_USER
+  		'user-agent': 'GitHub Avatar Downloader - Student Project'
   	}
   }
-  var res = request.get(requestOptions, cb);
+  request.get(requestOptions, cb);
 }
 
-getRepoContributors('MichaelMini','github-avatar-downloader', function(err, result){
+function downloadImageByURL (url, filePath){
+	if (!fs.existsSync('./avatars')) {
+		fs.mkdirSync('./avatars');
+	}
+	request.get(url)
+	       .on('error', function (err) {
+	         throw err;
+	       })
+	       .on('response', function (response) {
+	         console.log('Response Status Message: ', response.statusMessage);
+	         console.log('Response Content Type: ', response.headers['content-type']);
+	       })
+	       .pipe(fs.createWriteStream(filePath))
+
+	       // .on('finish', function () {
+	       // 	console.log('Download complete.')
+	       // })
+}
+
+function avatarsUrl(err, result){
 	var parseData = JSON.parse(result.body);
+			parseData.forEach(function(current_value, idx, array) {
+				var url = current_value.avatar_url;
+				var filePath = './avatars/' + current_value.login + '.jpg';
+				downloadImageByURL(url, filePath);
+				// if (idx === array.length - 1){
+		  //      var message = 'Download complete.';
+		  //      console.log(message);
+		  // 	}
+			});
+}
 
-	console.log(parseData[0]['avatar_url']);
-
-});
+getRepoContributors('jquery','jquery', avatarsUrl);
