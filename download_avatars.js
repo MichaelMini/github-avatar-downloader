2f3db278f75225514c6b053cf0690d872fc2512e
MichaelMini <mcho911@gmail.com>
2017-01-17 03:38:52 +0000

    Completed github-avatar-downloader

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,24 @@
 var request = require('request');
 
+var GITHUB_USER = "MichaelMini";
+var GITHUB_TOKEN = "c36ec186c9a3ad5104eb2dd2c83ba812b897bec9";
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestOptions = {
+  	url: requestURL,
+  	headers: {
+  		'user-agent': GITHUB_USER
+  	}
+  }
+  var res = request.get(requestOptions, cb);
+}
+
+getRepoContributors('MichaelMini','github-avatar-downloader', function(err, result){
+	var parseData = JSON.parse(result.body);
+
+	console.log(parseData[0]['avatar_url']);
+
+});
