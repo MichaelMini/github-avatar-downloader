8c5f616c70d0dd2420629dfc2c0677e115835058
MichaelMini <mcho911@gmail.com>
2017-01-18 02:20:50 +0000

    Config File for Secrets (Stretch) - finished

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,17 @@
 var request = require('request');
 var fs = require('fs');
-var GITHUB_USER = "MichaelMini";
-var GITHUB_TOKEN = "c36ec186c9a3ad5104eb2dd2c83ba812b897bec9";
+require('dotenv').config();
+var GITHUB_USER = process.env.GITHUB_USER;
+var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 var owner = process.argv[2];
 var name = process.argv[3];
+console.log(process.env.GITHUB_USER);
+console.log(process.env.GITHUB_TOKEN);
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log("ha ha ha", requestURL);
   var requestOptions = {
   	url: requestURL,
   	headers: {
@@ -37,20 +41,34 @@ function downloadImageByURL (url, filePath){
 }
 
 function avatarsUrl(err, result){
+	if (err) {
+		throw err;  // TODO: stop using this immature nonsense
+	}
 	var parseData = JSON.parse(result.body);
-			parseData.forEach(function(current_value, idx, array) {
-				var url = current_value.avatar_url;
-				var filePath = './avatars/' + current_value.login + '.jpg';
-				downloadImageByURL(url, filePath);
-				// if (idx === array.length - 1){
-		  //      var message = 'Download complete.';
-		  //      console.log(message);
-		  // 	}
-			});
+	if (parseData.message !== undefined) {
+		console.log("OH NO, GITHUB IS ANGRY!!!  They said: ", parseData.message);
+		if (parseData.documentation_url) {
+			console.log("Github suggested this documentation:", parseData.documentation_url);
+		}
+		process.exit(1);  // non-zero exit code means "something went wrong"
+		// throw JSON.stringify(parseData, null, 2);
+	}
+	parseData.forEach(function(current_value, idx, array) {
+		var url = current_value.avatar_url;
+		var filePath = './avatars/' + current_value.login + '.jpg';
+		downloadImageByURL(url, filePath);
+		// if (idx === array.length - 1){
+		// 	var message = 'Download complete.';
+		// 	console.log(message);
+		// }
+	});
 }
 
-if (!owner || !name) {
+if (!owner && !name) {
 	console.log('Please insert repoOwner and repoName while run again.')
 } else {
+	if (!owner) {
+		console.log('Please insert ')
+	}
 	getRepoContributors(owner, name, avatarsUrl);
 }
